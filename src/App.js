9641140776b09b96759e13455428a4be1080caa5
Nirob Hasan <nirob427417@gmail.com>
2022-11-09 11:21:29 +0600

    router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Main from './layout/Main';
 import Friends from './components/Friends/Friends';
+import FriendDetails from './components/FriendDetails/FriendDetails';
 
 function App() {
   const router = createBrowserRouter([
@@ -20,6 +21,13 @@ function App() {
             return fetch('https://jsonplaceholder.typicode.com/users');
           },
           element: <Friends></Friends>
+        },
+        {
+          path: '/friend/:friendId',
+          loader: async ({ params }) => {
+            return fetch(`https://jsonplaceholder.typicode.com/users/${params.friendId}`)
+          },
+          element: <FriendDetails></FriendDetails>
         }
 
       ]
---
+++ src/components/FriendDetails/FriendDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useActionData, useLoaderData } from 'react-router-dom';
+
+const FriendDetails = () => {
+    const friend = useLoaderData();
+    console.log(friend);
+    return (
+        <div>
+            <h1>Name:{friend.name}</h1>
+            <p>ph number:{friend.phone}</p>
+            <h2>everything you need to know</h2>
+        </div>
+    );
+};
+
+export default FriendDetails;
